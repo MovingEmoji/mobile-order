53a5c5b51e0303ca248809f9ff465032ece54e5f
MovingEmoji <mcnana814@gmail.com>
2024-08-29 22:33:58 +0900

    ステータス表示の実装

--- src/Ordered.jsx
+++ src/Ordered.jsx
@@ -1,10 +1,40 @@
 import { useCookies } from "react-cookie";
 import { QRCodeCanvas } from "qrcode.react";
 import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function Ordered(props) {
 
     const navigate = useNavigate();
+
+    const [status, setStatus] = useState("注文が完了しました。");
+
+    const axiosInstance = axios.create({
+        baseURL: "http://localhost:30003",
+        headers: {
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin": "*"
+        }
+    });
+
+    useEffect(() => {
+        function getStatus() {
+            axiosInstance.post("/status", cookies.orderID)
+                .then(res => {
+                    if(res.data === "pending") {
+                        setStatus("商品を準備しています。")
+                    } else if(res.data === "complete") {
+                        setStatus("商品が完成しました、お店へお越しください。")
+                    }
+                });
+        }
+        const interval = setInterval(() => {
+            getStatus();
+          }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
 
     function handleCick() {
         removeCookie("orderID");
@@ -29,6 +59,7 @@ function Ordered(props) {
                     />
                     <p>注文ID</p>
                     <p className="Small-Wrapper" style={{ marginBottom : 30 + 'px'}}>{cookies.orderID}</p>
+                    <p className="Small-Wrapper" style={{ marginBottom : 30 + 'px'}}>{ status }</p>
                     <p className="Small-Wrapper">続けて注文する場合は<br></br>画面をスクリーンショットしてください</p>
                     <div className="Info-Wrapper">
                         <button className="Button-Wrapper" style={{ marginInlineEnd : 5 + 'px', paddingBlock : 10 + 'px', backgroundColor : '#FF5050', width : '100%'}} onClick={handleCick}>続けて注文</button>
