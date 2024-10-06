43efbcc690ee19fa51a4ce4cf25ebfee7655e68a
MovingEmoji <mcnana814@gmail.com>
2024-10-07 01:55:40 +0900

    値段の変更

--- src/ItemList.jsx
+++ src/ItemList.jsx
@@ -3,8 +3,8 @@ function ItemList() {
     return(
         <main>
             <ItemCard image="noimage.jpg" title="パンケーキ" text="どこかで見たことあるようなパンケーキ<br/>１パック５枚" cost="100" id="1"/>
-            <ItemCard image="noimage.jpg" title="ワッフル" text="手のひらサイズのワッフル<br/>１パック２枚" cost="200" id="2"/>
-            <ItemCard image="noimage.jpg" title="だんご" text="コップに入った一口サイズのだんご<br/>１パック４個" cost="300" id="3"/>
+            <ItemCard image="noimage.jpg" title="ワッフル" text="手のひらサイズのワッフル<br/>１パック２枚" cost="100" id="2"/>
+            <ItemCard image="noimage.jpg" title="だんご" text="コップに入った一口サイズのだんご<br/>１パック４個" cost="100" id="3"/>
         </main>
     )
 }
